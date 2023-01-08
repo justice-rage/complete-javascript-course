9303cba39a0a2a55486e2f279fe7e4225887287f
justicepelteir <justice.douglas@hey.com>
2023-01-07 19:58:44 -0800

    complete lecture 131: functions accepting callback functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -61,3 +61,37 @@
 
 // Lecture 130: First-Class and Higher-Order Functions
 // Complete Lecture 130 video
+
+// ------------
+
+// // Lecture 131: Functions Accepting Callback Functions
+
+// const oneWord = function(str) {
+//     return str.replaceAll(' ', '').toLowerCase();
+// }
+
+// const upperFirstWord = function(str) {
+//     const [first, ...others] = str.split(' ');
+//     return [first.toUpperCase(), ...others].join(' ');
+// }
+
+// // Higher-order function
+// const transformer = function(str, fn) {
+//     console.log(`Original string: ${str}`);
+//     console.log(`Transformed string: ${fn(str)}`);
+
+//     console.log(`Transformed by: ${fn.name}`);
+// }
+
+// transformer('JavaScript is the best!', upperFirstWord);
+
+// transformer('JavaScript is the best!', oneWord);
+
+// // JS uses callbacks all the time
+// const high5 = function() {
+//     console.log('👋');
+// }
+
+// document.body.addEventListener('click', high5);
+
+// ['justice', 'martha', 'adam'].forEach(high5);
