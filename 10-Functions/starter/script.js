7b93fc09a6727ad9791825c5425e12e48ea4c058
justicepelteir <justice.douglas@hey.com>
2023-01-08 14:06:42 -0800

    complete lecture 136: immediately invoked function expressions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -357,3 +357,30 @@
 
 // // [5, 2, 3]
 // // [1, 5, 3, 9, 6, 1]
+
+// ------------
+
+// // Lecture 136: Immediately Invoked Function Expressions (IIFE)
+
+// const runOnce = function() {
+//     console.log('This will never run again');
+// };
+// runOnce();
+
+// // IIFE - Functions that are called immediately
+// (function(){
+//     console.log('This will never run again');
+//     const isPrivate = 23;
+// })();
+
+// // console.log(isPrivate);
+
+// (() => console.log('This will ALSO never run again'))();
+
+// {
+//     const isPrivate = 23;
+//     var notPrivate = 46;
+// }
+
+// // console.log(isPrivate);
+// console.log(notPrivate);
