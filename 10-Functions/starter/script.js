dac2e1282432c6ff34fcf0f68e06282763348aeb
justicepelteir <justice.douglas@hey.com>
2023-01-07 16:05:07 -0800

    complete lecture 128: default parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+// Lecture 128: Default Parameters
+
+const bookings = [];
+
+const createBooking = function(flightNum, numPassengers = 1, price = 1.99 * numPassengers) {
+    // ES5
+    // numPassengers = numPassengers || 1;
+    // price = price || 1.99;
+    const booking = {
+        flightNum,
+        numPassengers,
+        price
+    }
+    console.log(booking);
+    bookings.push(booking);
+}
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+createBooking('LH123', undefined, 500);
