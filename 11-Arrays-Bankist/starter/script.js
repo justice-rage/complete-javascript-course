6480eb3ca6df006c570e2744531ec78d3c96d4a5
justicepelteir <justice.douglas@hey.com>
2023-01-13 16:42:57 -0800

    complete lecture 151: computing usernames

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,22 +61,36 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-// const displayMovements = function(movements){
-//   containerMovements.innerHTML = '';
+const displayMovements = function(movements){
+  containerMovements.innerHTML = '';
 
-//   movements.forEach(function(mov, i){
-//     const type = mov > 0 ? 'deposit' : 'withdrawal';
+  movements.forEach(function(mov, i){
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
 
-//     const html = `
-//     <div class="movements__row">
-//       <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-//       <div class="movements__value">${mov}</div>
-//     </div>
-//     `;
-//     containerMovements.insertAdjacentHTML('afterbegin', html);
-//   });
-// };
-// displayMovements(account1.movements);
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+      <div class="movements__value">${mov}</div>
+    </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
+
+const user = 'Steven Thomas Williams'; // stw
+const createUsernames = function(accs){
+  accs.forEach(function(acc){
+  acc.username = acc.owner
+  .toLowerCase()
+  .split(' ')
+  .map(name => name[0])
+  .join('');
+  });
+};
+
+// createUsernames(accounts);
+// console.log(accounts);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -270,3 +284,8 @@ const inputClosePin = document.querySelector('.form__input--pin');
 //   (`Movement${i + 1}: You deposited ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(mov)}`));
 
 // console.log(movementDescriptions);
+
+// ------------
+
+// Lecture 151: Computing Usernames
+
