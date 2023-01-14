09537b03383785d55776004ecc89181cd1aa86f0
justicepelteir <justice.douglas@hey.com>
2023-01-13 16:55:02 -0800

    complete lecture 152: the filter method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -289,3 +289,24 @@ const createUsernames = function(accs){
 
 // Lecture 151: Computing Usernames
 
+// ------------
+
+// // Lecture 152: The filter Method
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// // const deposits = movements.filter(function(mov){
+// //   return mov > 0;
+// // });
+
+// const deposits = movements.filter(mov => mov > 0);
+
+// console.log(movements);
+// console.log(deposits);
+
+// const depositsFor = [];
+// for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+// console.log(depositsFor);
+
+// const withdrawals = movements.filter(mov => mov < 0);
+// console.log(withdrawals);
