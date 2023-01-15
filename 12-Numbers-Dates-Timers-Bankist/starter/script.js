34eace5d531753a1422c02efc664e7ae392fb158
justicepelteir <justice.douglas@hey.com>
2023-01-14 19:46:32 -0800

    complete lecture 180: timers: settimeout and setinterval

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -251,3 +251,24 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+// // Lecture 180: Timers: setTimeout and setInterval
+
+// // setTimeout
+// const ingredients  = ['olives', 'spinach'];
+
+// const pizzaTimer = setTimeout((ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2}!`),
+//  3000, 
+//  ...ingredients,
+//  );
+
+// console.log(`waiting...`);
+
+// if(ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+
+// // setInterval
+
+// setInterval(function(){
+//   const now = new Date();
+//   console.log(now);
+// }, 5000);
